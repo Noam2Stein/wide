cfef301bd7350e078f85a02463b290b56ced199f
Noam2Stein <noamsteindev@proton.me>
2026-07-25 16:58:40 +0300

    Add internal trait items for general-SIMD API

--- src/simd.rs
+++ src/simd.rs
@@ -18,7 +18,12 @@ pub trait SimdElement: Sealed + Pod {}
 /// Ideally, for any supported `T` type, all [`Simd<T, N>`] types would be
 /// supported. Unfortunately, that is impossible on stable Rust.
 #[expect(private_bounds)]
-pub trait SupportedSimd: SimdBackend {}
+pub trait SupportedSimd: SimdBackend {
+  /// The type returned by [`Simd::to_bitmask`].
+  ///
+  /// This is currently [`u32`] for all SIMD types.
+  type Bitmask;
+}
 
 /// Controls the internal implementation of [`Simd<T, N>`].
 ///
@@ -29,6 +34,72 @@ pub trait SupportedSimd: SimdBackend {}
 /// - `Inner` must have a size and alignment equal to `size_of::<T>() * N`
 pub(crate) unsafe trait SimdBackend {
   type Inner: Copy;
+
+  fn neg(self) -> Self;
+
+  fn not(self) -> Self;
+
+  fn add(self, rhs: Self) -> Self;
+
+  fn sub(self, rhs: Self) -> Self;
+
+  fn mul(self, rhs: Self) -> Self;
+
+  fn div(self, rhs: Self) -> Self;
+
+  fn rem(self, rhs: Self) -> Self;
+
+  fn bitand(self, rhs: Self) -> Self;
+
+  fn bitor(self, rhs: Self) -> Self;
+
+  fn bitxor(self, rhs: Self) -> Self;
+
+  fn simd_eq(self, other: Self) -> Self;
+
+  fn simd_ne(self, other: Self) -> Self;
+
+  fn simd_lt(self, other: Self) -> Self;
+
+  fn simd_gt(self, other: Self) -> Self;
+
+  fn simd_le(self, other: Self) -> Self;
+
+  fn simd_ge(self, other: Self) -> Self;
+
+  fn reduce_add(self) -> Self::T
+  where
+    Self: SimdAssociatedTypes;
+
+  fn reduce_mul(self) -> Self::T
+  where
+    Self: SimdAssociatedTypes;
+
+  fn bitselect(self, if_one: Self, if_zero: Self) -> Self;
+
+  fn select(self, if_true: Self, if_false: Self) -> Self;
+
+  fn to_bitmask(self) -> Self::Bitmask
+  where
+    Self: SupportedSimd;
+
+  fn any(self) -> bool;
+
+  fn all(self) -> bool;
+
+  fn transpose(matrix: Self::SelfxN) -> Self::SelfxN
+  where
+    Self: SimdAssociatedTypes;
+}
+
+/// A helper trait for [`SimdBackend`].
+///
+/// Since the [`SimdBackend`] trait is not generic over `T` and `N`, it needs
+/// this trait in order to have access to types that depend on those parameters.
+pub(crate) trait SimdAssociatedTypes {
+  type T;
+
+  type SelfxN;
 }
 
 /// Seals the [`SimdElement`] trait.
@@ -48,6 +119,16 @@ impl SimdElement for u32 {}
 impl SimdElement for u64 {}
 impl SimdElement for u128 {}
 impl SimdElement for usize {}
+
+impl<T, const N: usize> SimdAssociatedTypes for Simd<T, N>
+where
+  T: SimdElement,
+  Self: SupportedSimd,
+{
+  type T = T;
+
+  type SelfxN = [Self; N];
+}
 
 impl Sealed for f32 {}
 impl Sealed for f64 {}
